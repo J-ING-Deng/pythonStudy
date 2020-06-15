69c1702bac6f0985195f306a0a59c8ff2c90d347
dengjunyu <1083099314@qq.com>
2020-06-15 23:32:05 +0800

    异常

--- demo6.py
+++ demo6.py
@@ -1,10 +1,11 @@
 #异常
 
-a = "123"
-b = [1,2,3]
-try:
-    print(b[4])
-except:
-    print("出错了")
-finally:
-    print("没有错")
+# a = "123"
+# b = [1,2,3]
+# try:
+#     print(b[4])
+# except:
+#     print("出错了")
+# finally:
+#     print("没有错")
+
